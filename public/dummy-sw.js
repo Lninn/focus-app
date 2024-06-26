1644e13a3fc3b2e54c7e743749a547dcf6295514
Lninn <thl384458870@outlook.com>
2024-06-26 21:55:11 +0800

    move timer code to worker

--- public/dummy-sw.js
+++ public/dummy-sw.js
@@ -1,4 +1,7 @@
-console.log('++++++++++++++++')
+// const log = console.log.bind(console)
+const log = () => {}
+
+log('run dummy-sw.js...')
 
 self.addEventListener("fetch", event => {
   // This is a dummy event listener
@@ -6,26 +9,62 @@ self.addEventListener("fetch", event => {
   // some browsers
 });
 
-function start(m, asyncFn) {
-  let startTime = performance.now()
-  let total = m * 5 * 1000
+let flag = 0
+
+self.addEventListener('message', ev => {
+  const data = ev.data
+  log('[worker message event]', data)
+
+  const send = p => self.postMessage(p)
+
+  if (data.type === 'start') {
+    handleTimer(data.minute, send)
+  } else if (data.type === 'stop') {
+    log('stop timer', data, getState())
+    flag = 1;
+  }
+})
+
+function handleTimer(minute, sendFn) {
+
+  let previousSecond = 0
+  function onSecondChange(second) {
+    if (second === previousSecond) {
+      return
+    }
+    previousSecond = second
+    updateFor1Sec()
+
+    const state = getState()
+    sendFn({
+      type: 'interval',
+      msg: 'async data to main thread.',
+      state,
+    })
+  }
 
   let id
+  let startTime = performance.now()
+  let total = minute * 60 * 1000
   function loop() {
-    // console.log('loop...')
+    if (flag === 1) {
+      sendFn({
+        type: 'break',
+        msg: '提前終止計時器'
+      })
+      flag = 0;
+      cancelAnimationFrame(id)
+      return
+    }
 
     if (performance.now() > startTime + total) {
-      console.log('Foo end...')
-      asyncFn({
+      sendFn({
         type: 'end',
-        msg: 'time is end.'
+        msg: 'end of timer.'
       })
       cancelAnimationFrame(id)
       return
     }
-   
-    const percent = (performance.now() - startTime) / total
-    const progress = Math.floor(percent * 100)
 
     const currentSecond = Math.floor(
       (
@@ -33,28 +72,43 @@ function start(m, asyncFn) {
       ) / 1000
     )
 
-    console.log('progress:', {
-      percent,
-      progress,
-      currentSecond
-    })
+    onSecondChange(currentSecond)
 
     id = requestAnimationFrame(loop)
   }
+
+  initial(minute)
   id = requestAnimationFrame(loop)
 }
 
-self.addEventListener('message', ev => {
-  const data = ev.data
-  console.log('==*==', data)
-
-  if (data.type === 'click') {
-    start(1, (payload) => {
-      self.postMessage(payload)
-    })
+let second = 0
+let text = '00:00'
+function initial(minute) {
+  second = minute * 60
+}
+function updateFor1Sec() {
+  second--
+  if (second < 0) {
+    return false
   }
-})
+  const m = Math.floor(second / 60)
+  const s = second % 60
+  text = `${num2str(m)}:${num2str(s)}`
+  return true
+}
+function getState() {
+  return {
+    second,
+    text,
+  }
+}
 
-function notify(payload) {
-  postMessage(payload)
+function num2str(num) {
+  const str = String(num)
+  if (str.length === 1) {
+    return '0' + str
+  } else if (str.length > 2) {
+    throw new Error('too long number')
+  }
+  return str
 }
--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,13 @@ import { DEFAULT_SOUND_KEY, DEFAULT_SOUND_SRC, getSoundSrc } from './sound'
 import { getInterval } from './state'
 import useLocalState from './useLocalState'
 
+// const log = console.log.bind(console)
+const log = (...args: any) => { }
+
+function playSound() {
+  const raudio = new Audio(DEFAULT_SOUND_SRC)
+  raudio.play()
+}
 
 function App() {
 
@@ -14,63 +21,52 @@ function App() {
 
   const [soundkey, setSoundkey] = useLocalState('sound', DEFAULT_SOUND_KEY)
 
-  const timerRef = useRef<Timer>()
-
-  useEffect(() => {
-    timerRef.current = new Timer(
-      () => {
-        setIsRuning(false)
-      },
-      (t) => {
-        setText(t)
-      }
-    )
-  }, [])
+  const workerRef = useRef<Worker | null>(null);
 
   function handleMinuteChange(m: number) {
     setMinute(m)
   }
 
   function handleStart() {
-    const coreTimer = timerRef.current
-
-    if (!coreTimer) return
-
+    setIsRuning(!isRuning)
     if (isRuning) {
-      coreTimer.stop(true)
-    } else {
-      coreTimer.start(minitue)
+      const worker = workerRef.current
+      if (worker) {
+        worker.postMessage({ type: 'stop' })
+        workerRef.current = null
+      }
+      return
     }
 
-    setIsRuning(!isRuning)
+    const myWorker = new Worker('dummy-sw.js')
+    workerRef.current = myWorker;
+    
+    const payload = {
+      type: 'start',
+      msg: 'start a timer by value filed.',
+      minute: minitue,
+    }
+    myWorker.postMessage(payload)
+
+    myWorker.onmessage = ev => {
+      const data = ev.data
+      log('debug ', data)
+      if (data.type === 'interval') {
+        setText(data.state.text)
+      } else if (data.type === 'end') {
+        playSound()
+      } else if (data.type === 'break') {
+        // do nothing
+      }
+    }
   }
 
   function handleSoundChange(e: React.ChangeEvent<HTMLSelectElement>) {
-    const coreTimer = timerRef.current
-
-    if (!coreTimer) return
-
     const key = e.target.value
     setSoundkey(key)
-
-    const soundSrc = getSoundSrc(key)
-    coreTimer.changeSound(soundSrc)
   }
 
   const actText = isRuning ? '计时中...(再次点击中断计时)' : '点击开始计时'
-
-  function handleTestClick() {
-    const myWorker = new Worker('dummy-sw.js')
-    const payload = {
-      type: 'click',
-      msg: 'hello from react'
-    }
-    myWorker.postMessage(payload)
-    myWorker.onmessage = ev => {
-      const data = ev.data
-      console.log('debug ', data)
-    }
-  }
 
   return (
     <div className="App">
@@ -109,8 +105,6 @@ function App() {
       </div>
 
       <button onClick={handleStart}>{actText}</button>
-
-      <button onClick={handleTestClick}>启动一个1分钟定时</button>
     </div>
   )
 }
@@ -142,94 +136,6 @@ function QuickOptions({
       ))}
     </div>
   )
-}
-
-class Timer {
-  private id: number | null = null
-  private ins: MinuteUpdater | null = null
-
-  private endFn: () => void
-  private setText: (t: string) => void
-
-  private audio: HTMLAudioElement | null = null
-
-  constructor(endFn: () => void, setText: (t: string) => void) {
-    this.endFn = endFn
-    this.setText = setText
-
-    this.audio = new Audio(DEFAULT_SOUND_SRC)
-
-    // function onSoundLoad(ev: Event) {
-    //   console.log(ev);
-    // }
-    // this.audio.addEventListener('canplaythrough', onSoundLoad, false);
-  }
-
-  public start(minute: number) {
-    const __interval = getInterval()
-
-    const ins = new MinuteUpdater(minute)
-    this.ins = ins
-
-    this.id = setInterval(() => {
-      if (!ins.updateFor1Sec()) {
-        this.stop()
-      }
-
-      this.setText(ins.getText())
-    }, __interval)
-  }
-
-  public stop(isAhead = false) {
-    if (this.id) {
-      clearInterval(this.id)
-      this.id = null
-    }
-
-    this.ins = null
-
-    this.endFn()
-
-    this.setText('00:00')
-
-    if (!isAhead) {
-      this.audio?.play()
-    }
-  }
-
-  public changeSound(src: string) {
-    if (this.audio) {
-      this.audio.src = src
-    }
-  }
-}
-
-class MinuteUpdater {
-  private second: number
-  private text: string
-  constructor(min: number) {
-    this.second = min * 60
-
-    const _m = min < 9 ? '0' + min : min
-    this.text = `${_m}:00`
-  }
-
-  public updateFor1Sec() {
-    this.second--
-    if (this.second < 0) {
-      return false
-    }
-    const _m = Math.floor(this.second / 60)
-    const _s = this.second % 60
-    const m = _m < 9 ? '0' + _m : _m
-    const s = _s < 9 ? '0' + _s : _s
-    this.text = `${m}:${s}`
-    return true
-  }
-
-  public getText() {
-    return this.text
-  }
 }
 
 export default App
